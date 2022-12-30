009d67c6a84b87d5b5b80a9c47696549be73ad06
Sarah Hassan <sarah.hassan@target.com>
2022-12-30 11:09:48 -0600

    formatted images

--- src/App.js
+++ src/App.js
@@ -1,74 +1,46 @@
 import "./App.css";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import background from "./photo.jpg";
 import profilepic from "./pp.jpg";
-import { Image, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+// import { Typography } from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  // class App extends Component {
-  //   render() {
-  //     const myStyle = {
-  //       backgroundImage: `url(${background})`,
-  //       height: "100vh",
-  //       marginTop: "-70px",
-  //       fontSize: "50px",
-  //       backgroundSize: "cover",
-  //       backgroundRepeat: "no-repeat",
-  //     };
-
   return (
     <Container fluid>
-      {/* <div style={myStyle}>
-        <h1>GeeksForGeeks</h1>
-      </div> */}
-
-      {/* <Image width="1500" className="rounded mx-auto d-block" src="/.pp.jpg" /> */}
-      <Row className="justify-content-md-center">
+      <Row
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      >
         <Col xs={12} sm={4} md={4}>
           <img src={profilepic} width="370" height="500" alt="" />
         </Col>
       </Row>
 
-      {/* <div class="container-fluid">
-        <img src={profilepic} justify="right" width="370" height="500" alt="" />
-      </div> */}
+      <Container className="p-10">
+        <Row
+          style={{
+            flexDirection: "row-reverse",
+            justifyContent: "flex-end",
+          }}
+        >
+          <h1 className=".mb-14" style={{ fontSize: "5rem", color: "white" }}>
+            Frontend <div></div> Developer
+          </h1>
+          <h3 style={{ fontSize: "25px", color: "white" }}>
+            {" "}
+            I enjoy developing high-quality front-end products with excellent
+            user experiences.
+          </h3>
+        </Row>
+      </Container>
     </Container>
   );
 }
 
 export default App;
-
-// old react logo with linkedin hyperlink
-
-// return (
-
-// import logo from "./logo.svg";
-// import "./App.css";
-// import Container from "react-bootstrap/Container";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Container fixed>
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             hi! my name is <code>sarah.hassan</code> and welcome to my
-//             portfolio!
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://www.linkedin.com/in/sarah-hassan1/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             connect with me on LinkedIn
-//           </a>
-//         </header>
-//       </Container>
-//     </div>
-//   );
-// }
-
-// export default App;
