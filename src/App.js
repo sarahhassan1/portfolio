0e4ecba49046646347392dac04f8d0320abc256e
Sarah Hassan <sarah.hassan@target.com>
2023-01-03 13:56:40 -0600

    formatted images again

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import background from "./photo.jpg";
 import profilepic from "./pp.jpg";
 import { Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-// import { Typography } from "@material-ui/core";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Container fluid>
+    <Container className="m-5 p-5" fluid>
       <Row
         style={{
           flexDirection: "row",
-          justifyContent: "flex-end",
+          justifyContent: "center",
         }}
       >
-        <Col xs={12} sm={4} md={4}>
-          <img src={profilepic} width="370" height="500" alt="" />
-        </Col>
-      </Row>
-
-      <Container className="p-10">
-        <Row
-          style={{
-            flexDirection: "row-reverse",
-            justifyContent: "flex-end",
-          }}
-        >
-          <h1 className=".mb-14" style={{ fontSize: "5rem", color: "white" }}>
+        <Col>
+          <h1 className="mb-14" style={{ fontSize: "5rem", color: "white" }}>
             Frontend <div></div> Developer
           </h1>
-          <h3 style={{ fontSize: "25px", color: "white" }}>
+          <h3 className="mb-14" style={{ fontSize: "25px", color: "white" }}>
             {" "}
             I enjoy developing high-quality front-end products with excellent
             user experiences.
-          </h3>
-        </Row>
-      </Container>
+          </h3>{" "}
+        </Col>
+        <Col>
+          <img
+            className="profilepic"
+            src={profilepic}
+            width="370"
+            height="500"
+            alt=""
+          />
+        </Col>
+      </Row>
     </Container>
   );
 }
